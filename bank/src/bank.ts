e87b7d44636cd41f6e2c62d45451ad178c00d958
Eyder Rios <eyder.rios@gmail.com>
2025-03-20 10:00:49 -0300

    Aula 2025-03-20

--- bank/src/bank.ts
+++ bank/src/bank.ts
@@ -1,10 +1,43 @@
 
 class Bank {
-    id: number
-    name: string
+    private id: number
+    private name: string
 
-    constructor() {
-        this.id = 999
-        this.name = 'BANCO EXEMPLO S/A'
+    constructor(bankId: number, bankName: string) {
+        console.log('Bank()')
+
+        this.id = bankId
+        this.name = bankName
+    }
+
+    // GETTER
+    getId(): number {
+        return this.id
+    }
+
+    // SETTER
+    setId(id: number): void {
+        if(id > 0) {
+            this.id = id;
+        }
+    }
+
+    // GETTER
+    getName(): string {
+        return this.name
+    }
+
+    // SETTER
+    setName(name: string): void {
+        this.name = name
     }
 }
+
+const b1 = new Bank(990, 'BANCO SIMPLEX S/A')
+let b2 = new Bank(995, 'BANCO TOTAL S/A')
+
+b1.setId(104)
+b1.setName('CAIXA ECONÔMICA FEDERAL')
+
+console.log('Cod  Banco:', b1.getId())
+console.log('Nome Banco:', b1.getName())
