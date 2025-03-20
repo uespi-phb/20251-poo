fde495ca0e27b74028e4a253e318e865d9d39d5d
Eyder Rios <eyder.rios@gmail.com>
2025-03-20 08:06:48 -0300

    Aula 2025-03-18

---
+++ bank/src/bank.ts
@@ -0,0 +1,10 @@
+
+class Bank {
+    id: number
+    name: string
+
+    constructor() {
+        this.id = 999
+        this.name = 'BANCO EXEMPLO S/A'
+    }
+}
---
+++ hello/src/index2.ts
@@ -0,0 +1,5 @@
+let message: string
+
+message = 'Hello, world!'
+
+console.log(message)
